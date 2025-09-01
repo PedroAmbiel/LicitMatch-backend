70962bc1ed85badb7be85612e30bb279a27367f4
Pedro Ambiel Maschietto <pedro.maschietto@indaiatuba.sp.gov.br>
2025-09-01 08:28:41 -0300

    commit inicial
    - criação da estrutura base do projeto
    - configurações de banco de dados no application.properties

---
+++ src/main/java/br/com/match/licit/RestApplication.java
@@ -0,0 +1,8 @@
+package br.com.match.licit;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("LicitMatch/api/v1")
+public class RestApplication extends Application {
+}
